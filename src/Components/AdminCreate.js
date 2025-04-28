1a81ac09473da7235acb65826bcd890b3008b94d
ConnectVerse213 <Dev@Pranavs-MacBook-Air.local>
2025-04-28 23:46:35 +0530

    AdminCreate.js UI Modified

--- src/Components/AdminCreate.js
+++ src/Components/AdminCreate.js
@@ -462,10 +462,10 @@ function AdminCreate() {
        
       </div>
   <br></br>  <br></br>
-      <input style={{width:'4em' , fontSize: '13px',border:'2px solid white',color:'white' }} placeholder="50"></input>
+      <input style={{width:'4em' , fontSize: '40px',border:'2px solid white',color:'black' }} placeholder="50"></input>
       <br></br>
       <br></br>
-      <input style={{ fontSize: '13px',border:'2px solid white',color:'white' }} placeholder="50"></input>
+      <input style={{ fontSize: '40px',border:'2px solid white',color:'white' }} placeholder="50"></input>
     
       <br></br>
       <br></br>
