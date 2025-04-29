52c552ac2f7c331b06b41a2556fb7bca64510e7d
ConnectVerse213 <Dev@Pranavs-MacBook-Air.local>
2025-04-30 04:57:21 +0530

    EventPage.js UI Modified

--- src/Components/EventPage.js
+++ src/Components/EventPage.js
@@ -25,7 +25,7 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import coinImg from '../assets/images/coinImg.svg'
 import Alert from '@mui/material/Alert';
 import { ToastContainer, toast } from 'react-toastify';
-import './Home2.css'
+
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import EditCalendarIcon from '@mui/icons-material/EditCalendar';
 import Box from '@mui/material/Box';
@@ -81,6 +81,29 @@ function EventPage() {
 
    const [events, setEvents] = useState([]);
 
+   const [showAcceptInvite, setShowAcceptInvite] = useState(false);
+
+   function formatDate(dateStr) {
+    const date = new Date(dateStr);
+  
+    const day = date.getDate();
+    const month = date.toLocaleString('default', { month: 'short' }); // e.g., "Feb"
+    const year = date.getFullYear();
+  
+    // Get ordinal suffix for day
+    const getOrdinal = (n) => {
+      if (n > 3 && n < 21) return 'th';
+      switch (n % 10) {
+        case 1: return 'st';
+        case 2: return 'nd';
+        case 3: return 'rd';
+        default: return 'th';
+      }
+    };
+  
+    return `${day}${getOrdinal(day)} ${month}, ${year}`;
+  }
+
     const getEvents = async () => {
         const data = await getDocs(usersCollectionRef);
        
@@ -135,91 +158,159 @@ function EventPage() {
       
   return (
     <div>
-      <br></br>
-      <br></br>
-      <div className="full-width-bar" >
-        <div class="logo" >  <img src={logo} style={{width:'3em'}} alt="Logo" /></div>
-       
+      <div id="up"></div>
+  
+        <div className="item" >
 
-        <div style={{color:'white'}} >
+<div className="item1">
 
-        <Button  variant="outlined" onClick={()=>{
-          window.location.href="/home"
-        }} >Home</Button>
-      <Button  variant="contained"  >Event</Button>
-      
+<div class="item1a">
+
+<img class="poster" src={events.length!=0 && events[0].Image}></img>
+
+</div>
+<div class="item1b"> <l style={{color:'white'}}>Hosted by {events.length!=0 && events[0].Creator}</l>
 
 
-        </div>
+</div>
+
+<div class="item1c">
+  <l style={{color:'white'}}>Registrations: &nbsp;
+  {events.length!=0 && events[0].RegistrationsCount}</l>
+  
+  
+  
+
+  
+  </div>
+
+</div>
+
+
+<div className="item2">
+<div class="item2a">
+  
+  <h1 style={{color:'white'}}>{events.length!=0 && events[0].Name}</h1>
+<l style={{color:'white'}}>{events.length!=0 && formatDate(events[0].StartDateTime)}</l>
+
+<div style={{textAlign:'left'}}>
+{events.length!=0 && events[0].Address && <l style={{color:'white'}} >{events[0].Address}</l>}
+</div>
+
+<div>
+
+{events.length!=0 && events[0].Address && <iframe
+            title="Google Map"
+            src={`https://www.google.com/maps?q=${events[0].Address}&output=embed`}
+           class="map"
+           
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>}
+
+</div>
+
+<div>
+
+<a href="#up">
+      <Button variant="contained" type="submit" onClick={()=>{
+
+        setShowAcceptInvite(true)
+      }}>Accept Invitation</Button></a>
+</div>
+
+
+</div>
+
+<div class="item2b">
+ 
+<h1 style={{color:'white'}}>About Event</h1>
+
+
+<div style={{textAlign:'left'}}>
+
+
+
+{events.length!=0 && <div style={{textAlign:'left',color:'white'}} dangerouslySetInnerHTML={{ __html: events[0].Description }} />}
+</div>
+
+
+</div>
+    </div>
+    
+    </div>
+
+    {showAcceptInvite &&  <div style={{
+          width: '100%', 
+          height:'100%',
+         position:'sticky',
+          padding: '20px', 
+          backgroundColor: 'black', 
+          border: '2px solid #1876d1',
+          blur:'50px', 
+          textAlign: 'center', 
+          boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)', 
+          position: 'absolute', 
+          top: '0%', 
+          left: '50%', 
+          transform: 'translateX(-50%)',
+          zIndex: 9999,
+          animation: 'popupAnimation 0.5s ease',
+          display:'flex',
+          flexDirection:'column',
+          justifyContent:'center',
+          alignItems:'center'
+        }}>
           
-            <div className="text" > <Button variant="outlined" onClick={()=>{
-              showWidgetModal()
-            }}> <AccountBalanceWalletIcon/></Button></div>
-          </div>
-          <br></br>
-          <hr></hr>
-          <br></br>
-    <img style={{width:'20em'}} src={events.length!=0 && events[0].Image}></img>
-    <h1 style={{color:'white'}}> {events.length!=0 && events[0].Name}</h1>
-    {/* <h5 style={{color:'#1876d1'}}>Created by {events.length!=0 && events[0].Creator}</h5> */}
-  
-  
- 
-    <br></br>
-    {events.length!=0 && <div> <div class="alignIcons"><l style={{color:'#1876d1'}}><CalendarTodayIcon/> </l><l style={{color:'#1876d1'}}>Start: &nbsp; </l><l style={{color:'white'}}> {events.length!=0 && events[0].StartDateTime.slice(0,10)}</l>&nbsp;&nbsp;&nbsp;&nbsp; <l style={{color:'#1876d1'}}><AccessTimeIcon/> </l><l style={{color:'#1876d1'}}>Time: &nbsp; </l><l style={{color:'white'}}> {events.length!=0 && parseInt(events[0].StartDateTime.slice(11,13))>12 ? <l>{parseInt(events[0].StartDateTime.slice(11,13))-12}:{events[0].StartDateTime.slice(14)}&nbsp; PM</l>:<l>{parseInt(events[0].StartDateTime.slice(12,13))}:{events[0].StartDateTime.slice(14)}&nbsp; AM </l> }</l></div>
-  
-    
-  <br></br> 
-  <div class="alignIcons"><l style={{color:'#1876d1'}}><CalendarTodayIcon/> </l><l style={{color:'#1876d1'}}>End: &nbsp; </l><l style={{color:'white'}}> {events.length!=0 && events[0].EndDateTime.slice(0,10)}</l> &nbsp;&nbsp;&nbsp;&nbsp; <l style={{color:'#1876d1'}}><AccessTimeIcon/>  </l><l style={{color:'#1876d1'}}>Time: &nbsp; </l><l style={{color:'white'}}> {events.length!=0 && parseInt(events[0].EndDateTime.slice(11,13))>12 ? <l>{parseInt(events[0].EndDateTime.slice(11,13))-12}:{events[0].EndDateTime.slice(14)}&nbsp; PM</l>:<l>{parseInt(events[0].EndDateTime.slice(12,13))}:{events[0].EndDateTime.slice(14)}&nbsp; AM </l> }</l> </div>
-  <br></br>
- 
-  <div class="alignIcons"><l style={{color:'#1876d1'}}><LocationPinIcon/> </l><l style={{color:'#1876d1'}}>Location: &nbsp; </l><l style={{color:'white'}}> {events.length!=0 && events[0].Address.slice(events[0].Address.lastIndexOf(",") + 1)} </l><l onClick={()=>{
-    window.location.href=`https://www.google.com/maps?q=${events[0].Address}`
-  }}style={{color:'white'}}>(View in Google Maps)</l><img src={mapImage} style={{width:'2em'}} onClick={()=>{
-    window.location.href=`https://www.google.com/maps?q=${events[0].Address}`
-  }}></img></div></div>
-    
-    
-    }
-   <br></br>
-   <br></br>
-    <l style={{color:'#1876d1', fontSize:'24px'}}>About the event</l>
-    <br></br>
-    <center>
-    {events.length!=0 && <p style={{color:'white',maxWidth:'500px'}}>{events[0].Description}</p>}
-    </center>
-    <br></br>  <br></br>
-    <hr></hr>
-
-    <br></br><br></br>
-    <br></br>
-    <form onSubmit={handleSubmit}>
-      {events.length!=0 && events[0].Questions.map((question, index) => (
-        <div key={index} style={{ marginBottom: '15px',color:'white' }}>
-          <label style={{color:'#1876d1'}}>{question}</label>
-          <br />
          
-          <input  class="form__field"
+          <br></br>
+          <center>
+          <form onSubmit={handleSubmit} style={{backgroundColor:'black',color:'white'}}>
+      {events.length!=0 && events[0].Questions.map((question, index) => (
+        <div key={index} style={{ marginBottom: '15px',color:'white'}}>
+          <label >{question}</label>
+          <br /><br></br>
+
+        
+          <input  
             type="text" 
-            value={answers[index]}
+            className='custom-input'
+            style={{fontSize:'36px',maxWidth:'70%',borderTop:'none',borderLeft:'none',borderRight:'none',backgroundColor:'black',color:'white'}}
+           placeholder={answers[index]}
+           
+            
             onChange={(e) => handleChange(index, e.target.value)}
-            style={{maxWidth:'350px'}}
+            
             
           />
+
+
+
+
         </div>
       ))}
        <br></br>
      
        <br></br>
-      <button class="btn4" type="submit">Register</button>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-     
-     
 
-    </form>
+       <Button variant="outlined" style={{border:'1px solid red',color:'red'}}onClick={()=>{
+        setShowAcceptInvite(false)
+      }}>cancel</Button>
+      &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <Button  type="submit" variant="contained">Register</Button>
+      
+      </form>
+
+      
+          </center>
+          
+          <div >
+         
+              </div>
+     <br></br><br></br>
+         
+       
+        </div>}
     </div>
   )
 }
